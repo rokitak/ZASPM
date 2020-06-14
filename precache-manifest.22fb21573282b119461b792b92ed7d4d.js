76e4c7754d55dc7596a4ca80370a4e412e22bff2
Krystian Rokita <krokita@consol.de>
2020-06-14 23:38:11 +0200

    Updates

---
+++ precache-manifest.22fb21573282b119461b792b92ed7d4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d558fbfe3af4d66e762971c970eca267",
+    "url": "/ZASPM/index.html"
+  },
+  {
+    "revision": "f9e13f00480aa13a0e72",
+    "url": "/ZASPM/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "f9e13f00480aa13a0e72",
+    "url": "/ZASPM/static/js/2.008c4c3f.chunk.js"
+  },
+  {
+    "revision": "e94add418d9ba2324972",
+    "url": "/ZASPM/static/js/main.8181b88d.chunk.js"
+  },
+  {
+    "revision": "77b5edfe78c822f004ce",
+    "url": "/ZASPM/static/js/runtime~main.2a69f635.js"
+  }
+]);
